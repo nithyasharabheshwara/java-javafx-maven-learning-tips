8bbe60d728c65f056a2265da2b1b3859c10d9e8d
Shashank Tulsyan <shashaanktulsyan@users.noreply.github.com>
2020-04-28 19:21:40 +0530

    Answering Adesh Singh's stackoverflow question
    https://stackoverflow.com/questions/61460446/unable-to-display-video-in-webpage-using-javafx-webview-video-is-live-on-the-gi

---
+++ nu-sharabheshwara-adesh_singh-webview/src/main/java/nu/sharabheshwara/adesh_singh/webviewbrowser/WebViewBrowser.java
@@ -0,0 +1,90 @@
+package nu.sharabheshwara.adesh_singh.webviewbrowser;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.List;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class WebViewBrowser extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = new WebViewPane();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        primaryStage.setScene(new Scene(root, width, height));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public class WebViewPane extends Pane {
+
+        public WebViewPane() {
+            VBox.setVgrow(this, Priority.ALWAYS);
+            setMaxWidth(Double.MAX_VALUE);
+            setMaxHeight(Double.MAX_VALUE);
+
+            WebView view = new WebView();
+            view.setMinSize(500, 400);
+            view.setPrefSize(500, 400);
+            final WebEngine eng = view.getEngine();
+            try {
+                eng.load("http://techyadesh.com/video.php");
+            } catch (Exception ex) {
+
+            }
+
+            GridPane grid = new GridPane();
+            grid.setVgap(5);
+            grid.setHgap(5);
+            GridPane.setConstraints(view, 0, 1, 2, 1, HPos.CENTER, VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
+            grid.getColumnConstraints().addAll(
+                    new ColumnConstraints(100, 100, Double.MAX_VALUE, Priority.ALWAYS, HPos.CENTER, true),
+                    new ColumnConstraints(40, 40, 40, Priority.NEVER, HPos.CENTER, true)
+            );
+            // grid.getChildren().addAll(locationField, goButton, view);
+            grid.getChildren().addAll(view);
+            getChildren().add(grid);
+        }
+
+        @Override
+        protected void layoutChildren() {
+            List<Node> managed = getManagedChildren();
+            double width = getWidth();
+            double height = getHeight();
+            double top = getInsets().getTop();
+            double right = getInsets().getRight();
+            double left = getInsets().getLeft();
+            double bottom = getInsets().getBottom();
+            for (int i = 0; i < managed.size(); i++) {
+                Node child = managed.get(i);
+                layoutInArea(child, left, top,
+                        width - left - right, height - top - bottom,
+                        0, Insets.EMPTY, true, true, HPos.CENTER, VPos.CENTER);
+            }
+        }
+    }
+}
